8647a3cbe5ea6d5a7905a9f376eb13ff31a3a86d
NH1VII <niskarki001@gmail.com>
2025-12-15 21:34:12 +0545

    cha

---
+++ lua/plugins/gitTools.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "tpope/vim-fugitive"
+    },
+
+    {
+        "lewis6991/gitsigns.nvim",
+
+        config = function()
+            require("gitsigns").setup()
+
+            vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_line_blame<CR>")
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns previes_hunk<CR>", {})
+        end
+    }  
+
+}
+
+
